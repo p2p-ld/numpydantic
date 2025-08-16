b20596eb0f9b45c2538b92ad2b32c9417201d411
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-08-15 19:49:41 -0700

    extreme hack ndarray as protocol

--- src/numpydantic/testing/helpers.py
+++ src/numpydantic/testing/helpers.py
@@ -129,7 +129,7 @@ class ValidationCase(BaseModel):
     """
     String identifying the validation case
     """
-    annotation_shape: tuple[int | str, ...] | tuple[tuple[int | str, ...], ...] = (
+    annotation_shape: None | tuple[int | str, ...] | tuple[tuple[int | str, ...], ...] = (
         10,
         10,
         "*",
@@ -163,9 +163,13 @@ class ValidationCase(BaseModel):
         Annotation used in the model we validate against
         """
         # make a union type if we need to
-        shape_union = all(
-            isinstance(s, Sequence) and not isinstance(s, str)
-            for s in self.annotation_shape
+        shape_union = (
+            False
+            if self.annotation_shape is None
+            else all(
+                isinstance(s, Sequence) and not isinstance(s, str)
+                for s in self.annotation_shape
+            )
         )
         dtype_union = isinstance(self.annotation_dtype, Sequence) and all(
             isinstance(s, Sequence) for s in self.annotation_dtype
@@ -179,13 +183,19 @@ class ValidationCase(BaseModel):
             )
             annotations: list[type] = []
             for shape, dtype in product(shape_iter, dtype_iter):
-                shape_str = ", ".join([str(i) for i in shape])
-                annotations.append(NDArray[Shape[shape_str], dtype])
+                if shape is None:
+                    annotations.append(NDArray[Any, dtype])
+                else:
+                    shape_str = ", ".join([str(i) for i in shape])
+                    annotations.append(NDArray[Shape[shape_str], dtype])
             return Union[tuple(annotations)]  # noqa: UP007
 
         else:
-            shape_str = ", ".join([str(i) for i in self.annotation_shape])
-            return NDArray[Shape[shape_str], self.annotation_dtype]
+            if self.annotation_shape is None:
+                return NDArray[Any, self.annotation_dtype]
+            else:
+                shape_str = ", ".join([str(i) for i in self.annotation_shape])
+                return NDArray[Shape[shape_str], self.annotation_dtype]
 
     @computed_field()
     def model(self) -> type[BaseModel]:
