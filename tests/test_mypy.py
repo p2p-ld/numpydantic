5b27b4b8036b9b8111c0783bbf5064d5c7c39281
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-06-01 17:28:25 -0700

    move existing mypy and pyright tests to a stub-only test directory

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -3,16 +3,18 @@ from pathlib import Path
 import mypy.api
 import pytest
 
-from numpydantic import ndarray
-from numpydantic.meta import update_ndarray_stub
+from numpydantic import ndarray, update_ndarray_stub
+from numpydantic.testing.cases import MYPY_TYPING_CASES
+from numpydantic.testing.surfaces import AnnotationForm
 
-MYPY_DIR = Path(__file__).parent / "data" / "mypy"
-
+DATA_DIR = Path(__file__).parent / "data"
+MYPY_DIR = DATA_DIR / "mypy"
 
 pytestmark = pytest.mark.typechecking
 
 
-@pytest.fixture(scope="session", autouse=True)
+
+@pytest.fixture(scope="session")
 def refresh_stubs():
     """Ensure no stale stubs"""
     # ensure all interfaces are imported
@@ -30,10 +32,18 @@ def refresh_stubs():
         backup_file.rename(stub_file)
 
 
-@pytest.mark.parametrize("test_file", MYPY_DIR.glob("*.py"))
-def test_mypy(test_file: Path):
-    """The mypy examples should pass static type checking"""
-    res = mypy.api.run([str(test_file)])
+@pytest.fixture
+def blank_config(tmp_path) -> Path:
+    """Nothing to see here, just a blank file without the plugins configured"""
+    config_path = tmp_path / "pyproject.toml"
+    config_path.write_text("[tool.mypy]")
+    return config_path
+
+
+@pytest.mark.parametrize("test_file", (DATA_DIR / "stub" / "correct").glob("*.py"))
+def test_mypy_noplugin(test_file: Path, refresh_stubs, blank_config):
+    """The mypy examples should pass static type checking with only the stub"""
+    res = mypy.api.run(["--config-file", str(blank_config), str(test_file)])
     assert "Success: no issues found in 1 source file" in res[0]
     assert res[1] == ""
     assert res[2] == 0
--- tests/test_pyright.py
+++ tests/test_pyright.py
@@ -4,15 +4,20 @@ from pathlib import Path
 import pyright
 import pytest
 
-MYPY_DIR = Path(__file__).parent / "data" / "mypy"
+DATA_DIR = Path(__file__).parent / "data"
 
 pytestmark = pytest.mark.typechecking
 
 
-@pytest.mark.parametrize("test_file", MYPY_DIR.glob("*.py"))
-def test_pyright(test_file: Path):
-    """The mypy examples should pass static type checking with pyright too"""
+@pytest.mark.parametrize(
+    "test_file", sorted((DATA_DIR / "stub" / "correct").glob("*.py"))
+)
+def test_pyright(test_file: Path) -> None:
+    """The mypy examples should pass static type checking with pyright too."""
     res = pyright.run(
-        "--outputjson", str(test_file), stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        "--outputjson",
+        str(test_file),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
     assert res.returncode == 0, f"stderr:\n{res.stderr}\n\nstdout:\n{res.stdout}"
